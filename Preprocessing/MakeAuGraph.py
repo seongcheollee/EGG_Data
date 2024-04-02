a35e8cc05cf17e6e8e09335debff564c0ca23f8f
seongcheollee <jhk6520736@gmail.com>
2024-04-02 18:36:58 +0900

    리팩토링

--- Preprocessing/MakeAuGraph.py
+++ Preprocessing/MakeAuGraph.py
@@ -1,79 +1,86 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
-from pyspark.sql.functions import explode_outer,explode, col, when, size,concat,array, first,arrays_zip
-from pyspark.sql.functions import udf, explode, collect_list, size, expr, struct, col, when, array_contains, flatten
-from pyspark.sql.types import StringType, ArrayType, StructType, StructField, DoubleType
+import pyspark.sql.types as T
 import numpy as np
 import pandas as pd
 import ast
 import networkx as nx
-from pymongo import MongoClient
 import datetime
 
 # SparkSession을 생성
-spark = SparkSession.builder.appName("PreprocessingSpark").getOrCreate()
-
-# 몽고디비 클라이언트 연결
-
-client = MongoClient('mongodb://ditto:AbBaDittos!230910*@localhost', 27017)
+spark = SparkSession.builder\
+    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0")\
+    .appName("egg").getOrCreate()
 
 # 날짜
 current_datetime = datetime.datetime.now()
 year = current_datetime.year
 month = current_datetime.month
-one_month_ago = current_datetime - datetime.timedelta(days=current_datetime.day)
-one_month_ago = one_month_ago.month
-
-pd.DataFrame.iteritems = pd.DataFrame.items
+one_month_ago = (current_datetime - datetime.timedelta(days=current_datetime.day)).month
 
 # UDF 정의
 def parse_lists(list_str):
     return ast.literal_eval(list_str) if list_str.strip() else ['None']
 
-def parse_ems(ems):
-    ems = ems.strip('[]').split()
-    return [float(val) for val in ems]
-
 # UDF 등록
-parse_lists_udf = udf(parse_lists, ArrayType(StringType()))
-parse_ems_udf = udf(parse_ems, ArrayType(DoubleType()))
+parse_lists_udf = F.udf(parse_lists, T.ArrayType(T.StringType()))
 
-
-def get_kci_data(df_name):
-    print('Get',df_name,"!")
-    kci_db_name = "kci_trained_api"
-    kci_db = client[kci_db_name]
-    kci_data = list(kci_db[df_name].find({}))
+def get_kci_data(db_name, col_name):
     
-    pandas_df = pd.DataFrame(kci_data)
-    print(pandas_df.columns)
-    pandas_df = pandas_df[['articleID','titleKor','journalID','journalName','issn','citations','pubYear','author1ID','author1Name','author1Inst','author2IDs','author2Names','author2Insts', 'class','keywords', 'ems']]
+    print('Get ',col_name,"!")
 
-    pandas_df = pandas_df.astype(str)
+    schema = T.StructType([
+        T.StructField("articleID", T.StringType(), True),
+        T.StructField("titleKor", T.StringType(), True),
+        T.StructField("journalID", T.StringType(), True),
+        T.StructField("journalName", T.StringType(), True),
+        T.StructField("issn", T.StringType(), True),
+        T.StructField("citations", T.StringType(), True),
+        T.StructField("pubYear", T.StringType(), True),
+        T.StructField("author1ID", T.StringType(), True),
+        T.StructField("author1Name", T.StringType(), True),
+        T.StructField("author1Inst", T.StringType(), True),
+        T.StructField("author2IDs", T.StringType(), True),
+        T.StructField("author2Names", T.StringType(), True),
+        T.StructField("author2Insts", T.StringType(), True),
+        T.StructField("class", T.StringType(), True),
+        T.StructField("keywords",T.StringType(), True)
+    ])
 
-    spark_df = spark.createDataFrame(pandas_df)
-    spark_df = spark_df.withColumn("keywords", parse_lists_udf(spark_df['keywords']))
-    spark_df = spark_df.withColumn("ems", parse_ems_udf(spark_df["ems"]))
-    spark_df = spark_df.withColumn("author2IDs", parse_lists_udf(spark_df["author2IDs"]))
-    spark_df = spark_df.withColumn("author2Names", parse_lists_udf(spark_df["author2Names"]))
-    spark_df = spark_df.withColumn("author2Insts", parse_lists_udf(spark_df["author2Insts"]))
+    kci_api_uri = f"mongodb://{config.mongo_user}:{config.mongo_pass}@mongodb:27017/{db_name}.{col_name}?authSource=admin"
 
-    return spark_df
+    df = spark.read.format("mongo") \
+        .option("uri", kci_api_uri) \
+        .schema(schema)\
+        .load()
+        
+    df = df.withColumn("author2IDs", parse_lists_udf(df["author2IDs"]))
+    df = df.withColumn("author2Names", parse_lists_udf(df["author2Names"]))
+    df = df.withColumn("author2Insts", parse_lists_udf(df["author2Insts"]))
+    df = df.withColumn("keywords", parse_lists_udf(df["keywords"]))
 
+    return df
 
-def get_kci_auInfo_data(df_name):
-    print('Get',df_name,"!")
-    kci_db_name = "kci_author_info"
-    kci_db = client[kci_db_name]
-    kci_data = list(kci_db[df_name].find({}))
+def get_author_data(db_name, col_name):
+    
+    print('Get ',col_name,"!")
 
-    pandas_df = pd.DataFrame(kci_data)
-    print(pandas_df.columns)
-    pandas_df = pandas_df[['authorID', 'kiiscArticles','totalArticles', 'if', 'H-index']]
-    pandas_df = pandas_df.astype(str)
-    spark_df = spark.createDataFrame(pandas_df)
+    schema = T.StructType([
+        T.StructField("authorID", T.StringType(), True),
+        T.StructField("kiiscArticles", T.StringType(), True),
+        T.StructField("totalArticles", T.StringType(), True),
+        T.StructField("if", T.StringType(), True),
+        T.StructField("H-index", T.StringType(), True)
+    ])
 
-    return spark_df
+    kci_api_uri = f"mongodb://{config.mongo_user}:{config.mongo_pass}@mongodb:27017/{db_name}.{col_name}?authSource=admin"
+
+    df = spark.read.format("mongo") \
+        .option("uri", kci_api_uri) \
+        .schema(schema)\
+        .load()
+
+    return df
 
 def merge_df(origin_df , new_df):
     print("Merge!")
@@ -82,16 +89,14 @@ def merge_df(origin_df , new_df):
 
 def exploded_df(df):
     print('Exploded!')
-    exploded_df = df.withColumn("temp", explode_outer(arrays_zip("author2IDs", "author2Names", "author2Insts"))) \
+    df = df.withColumn("temp", F.explode_outer(F.arrays_zip("author2IDs", "author2Names", "author2Insts"))) \
         .drop("author2IDs", "author2Names", "author2Insts") \
         .selectExpr("*", "temp.*") \
         .withColumnRenamed("author2IDs", "author2ID") \
         .withColumnRenamed("author2Names", "author2Name") \
         .withColumnRenamed("author2Insts", "author2Inst") \
         .drop("temp")
-        
-    print(exploded_df.columns)
-    result_df = exploded_df.select(
+    df = df.select(
         "articleID",
         "titleKor",
         "journalID",
@@ -106,12 +111,10 @@ def exploded_df(df):
         "author2Name",
         "author2Inst",
         'class',
-        'keywords',
-        'ems'
+        'keywords'
         )
-#    result_df = result_df.dropDuplicates(["articleID", "author1ID","author1Name", "author2ID","author2Name"])
-    return result_df
 
+    return df
 
 def joined_df(df):
     print("Outer join")
@@ -135,7 +138,6 @@ def joined_df(df):
         "citations",
         "class",
         "keywords",
-        "ems"
     )
     joined_df = merged_df.join(selected_columns, merged_df.authorID == selected_columns.author1ID, "outer")
 
@@ -144,20 +146,19 @@ def joined_df(df):
 def grouping(df):
     print("Grouping")
     grouped_df = df.groupBy("authorID", "author1Name","author1Inst").agg(
-        collect_list("articleID").alias("articleIDs"),
-        collect_list("titleKor").alias("titleKor"),
-        collect_list("author2ID").alias("with_author2IDs"),
-        collect_list("author1ID").alias("with_author1IDs"),
-        collect_list("citations").alias("citations"),
-        collect_list("journalID").alias("journalIDs"),
-        collect_list("pubYear").alias("pubYears"),
-        collect_list("class").alias("class"),
-        collect_list("keywords").alias("word_cloud"),
-        first("ems").alias("ems")
+        F.collect_list("articleID").alias("articleIDs"),
+        F.collect_list("titleKor").alias("titleKor"),
+        F.collect_list("author2ID").alias("with_author2IDs"),
+        F.collect_list("author1ID").alias("with_author1IDs"),
+        F.collect_list("citations").alias("citations"),
+        F.collect_list("journalID").alias("journalIDs"),
+        F.collect_list("pubYear").alias("pubYears"),
+        F.collect_list("class").alias("class"),
+        F.collect_list("keywords").alias("word_cloud"),
 
     )
-    grouped_df = grouped_df.withColumn("with_author1IDs", array())
-    grouped_df = grouped_df.withColumn("word_cloud", flatten(grouped_df["word_cloud"]))
+    grouped_df = grouped_df.withColumn("with_author1IDs", F.array())
+    grouped_df = grouped_df.withColumn("word_cloud", F.flatten(grouped_df["word_cloud"]))
 
     return grouped_df
     
@@ -171,16 +172,12 @@ def join_author_info(grouped_df, author_info_df):
     joined_df = joined_df.withColumnRenamed("class", "category")
     return joined_df
 
-
-def save_final_kci_data(final_df):
-    final = final_df.toPandas()
-    output_col_name = "kci_AuGraph_{:04d}{:02d}".format(year, month)
-    db = client.get_database('kci_AuGraph')
-    cl = db.get_collection(output_col_name)
-    final = final.to_dict('records')
-    cl.insert_many(final)
-    print("saved ",output_col_name,"!")
-
+def save_final_kci_data(df, db_name, col_name):
+    save_uri = f"mongodb://{config.mongo_user}:{config.mongo_pass}@mongodb:27017/{db_name_}.{col_name}?authSource=admin"
+    df.write.format("mongo") \
+        .option("uri", save_uri) \
+        .mode("overwrite") \
+        .save()
 
 def generate_and_save_graph(df):
     print("Generate AuGraph")
@@ -203,17 +200,23 @@ def generate_and_save_graph(df):
     print('Saved', graphname)
 
 
+
+kci_db = "kci_trained_api"
+au_db = "kci_author_info"
+kau_db = "kci_AuGraph"
+
 previous_col_name = "kci_trained_{:04d}{:02d}".format(year, one_month_ago)
 current_col_name = "kci_trained_{:04d}{:02d}".format(year, month)
-previous_col_name_author_info = "author_{:04d}{:02d}".format(year, month)
+previous_col_name_author_info = "author_{:04d}{:02d}".format(year, one_month_ago)
+current_col_name_author_info = "kci_AuGraph_{:04d}{:02d}".format(year, month)
 
-origin_df = get_kci_data(previous_col_name)
-new_df = get_kci_data(current_col_name)
-author_df = get_kci_auInfo_data(previous_col_name_author_info)
+origin_df = get_kci_data(kci_db,previous_col_name)
+new_df = get_kci_data(kci_db, current_col_name)
+author_df = get_author_data(au_db,previous_col_name_author_info)
 merged_df = merge_df(origin_df, new_df)
 exploded_df = exploded_df(merged_df)
 joined_df = joined_df(exploded_df)
 grouped_df = grouping(joined_df)
 final_df = join_author_info(grouped_df, author_df)
-#save_final_kci_data(final_df)
+save_final_kci_data(final_df,kau_db  ,current_col_name_author_info)
 #generate_and_save_graph(grouped_df)
